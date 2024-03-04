3796b8b0e93b14b97c02517ad747e89abfaf0ed8
kimpangya <nam74415133@gmail.com>
2024-03-04 11:29:04 +0900

    🎉 init commit

---
+++ src/main/java/com/sparta/njick/NjickApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.njick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/njick/NjickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.njick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

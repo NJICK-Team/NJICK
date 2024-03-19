bdea6492e745a4e7f0620be76d3cf9946bffc65d
jinkshower <askthem@naver.com>
2024-03-19 14:02:36 +0900

    feat: card service 생성 메서드 구현

--- src/main/java/com/sparta/njick/domain/card/repository/CardRepositoryImpl.java
+++ src/main/java/com/sparta/njick/domain/card/repository/CardRepositoryImpl.java
@@ -1,14 +1,29 @@
 package com.sparta.njick.domain.card.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.njick.domain.card.infrastructure.entity.CardEntity;
 import com.sparta.njick.domain.card.infrastructure.entity.CardJpaRepository;
+import com.sparta.njick.domain.card.model.Card;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @RequiredArgsConstructor
+@Repository
 public class CardRepositoryImpl implements CardRepository {
 
     private final CardJpaRepository cardJpaRepository;
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Card save(Card model) {
+        CardEntity entity = CardEntity.builder()
+            .title(model.getTitle())
+            .description(model.getDescription())
+            .cardColor(model.getCardColor())
+            .deadline(model.getDeadline())
+            .boardId(model.getBoardId())
+            .taskStateId(model.getTaskStateId())
+            .build();
+        return cardJpaRepository.save(entity).toModel();
+    }
 }
--- src/main/java/com/sparta/njick/domain/card/service/CardService.java
+++ src/main/java/com/sparta/njick/domain/card/service/CardService.java
@@ -1,8 +1,28 @@
 package com.sparta.njick.domain.card.service;
 
+import com.sparta.njick.domain.card.dto.request.CardCreateRequestDto;
+import com.sparta.njick.domain.card.dto.response.CardResponseDto;
+import com.sparta.njick.domain.card.model.Card;
+import com.sparta.njick.domain.card.repository.CardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CardService {
 
+    private final CardRepository cardRepository;
+
+    public CardResponseDto createCard(CardCreateRequestDto requestDto, Long boardId) {
+        Card card = Card.builder()
+            .title(requestDto.getTitle())
+            .description(requestDto.getDescription())
+            .cardColor(requestDto.getCardColor())
+            .deadline(requestDto.getDeadline())
+            .taskStateId(requestDto.getTaskStateId())
+            .boardId(boardId)
+            .build();
+        card = cardRepository.save(card);
+        return new CardResponseDto(card);
+    }
 }

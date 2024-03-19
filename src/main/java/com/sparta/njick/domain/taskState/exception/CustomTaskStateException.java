bce7c45809090c8c8cfb0762263ac4cf7fb063ec
kimpangya <nam74415133@gmail.com>
2024-03-19 22:01:58 +0900

    ✨  feat/taskStates : 작업상태 예외 메시지 코드

--- src/main/java/com/sparta/njick/domain/taskState/exception/CustomTaskStateException.java
+++ src/main/java/com/sparta/njick/domain/taskState/exception/CustomTaskStateException.java
@@ -1,5 +1,19 @@
 package com.sparta.njick.domain.taskState.exception;
 
-public class CustomTaskStateException {
+import org.springframework.http.HttpStatus;
 
+public class CustomTaskStateException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final String message;
+
+    public CustomTaskStateException(final TaskStateErrorCode errorCode) {
+        this.status = HttpStatus.BAD_REQUEST;
+        this.message = errorCode.getMessage();
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/com/sparta/njick/domain/taskState/exception/TaskStateErrorCode.java
+++ src/main/java/com/sparta/njick/domain/taskState/exception/TaskStateErrorCode.java
@@ -1,5 +1,15 @@
 package com.sparta.njick.domain.taskState.exception;
 
-public class TaskStateErrorCode {
+import lombok.Getter;
 
+@Getter
+public enum TaskStateErrorCode {
+
+    TASKSTATE_ERROR_CODE_NOT_FOUND("해당 작업상태를 찾을 수 없습니다."),
+    TASKSTATE_ERROR_CODE_BOARDID_MISMATCH("해당 작업상태는 이 보드에 존재하지 않습니다.");
+    private final String message;
+
+    TaskStateErrorCode(String message) {
+        this.message = message;
+    }
 }

cebd2dd3f14ca54256a59ed29be8fb6cb5cd3f11
Modafi <dkdpdldjdy0619@gmail.com>
2024-03-19 18:32:03 +0900

    feat: implement Board service feature
    - BoardService
    - BoardServiceImpl
    - BoardInfoDTO

--- src/main/java/com/sparta/njick/domain/board/service/BoardService.java
+++ src/main/java/com/sparta/njick/domain/board/service/BoardService.java
@@ -1,5 +1,18 @@
 package com.sparta.njick.domain.board.service;
 
+import com.sparta.njick.domain.board.controller.dto.request.BoardRegisterDTO;
+import com.sparta.njick.domain.board.controller.dto.request.DeleteBoardDTO;
+import com.sparta.njick.domain.board.controller.dto.request.UpdateBoardDTO;
+import com.sparta.njick.domain.board.service.dto.BoardInfoDTO;
+import com.sparta.njick.domain.board.controller.dto.request.BoardParticipateDTO;
+
+import java.util.List;
+
 public interface BoardService {
-    // TODO: implement Board service
+    BoardInfoDTO register(final BoardRegisterDTO dto);
+    void participate(final BoardParticipateDTO dto);
+    List<BoardInfoDTO> searchAllParticipateBoard(final Long boardId);
+    List<BoardInfoDTO> searchAllOwnedBoard(final Long creatorId);
+    BoardInfoDTO update(final UpdateBoardDTO dto);
+    void delete(final DeleteBoardDTO dto);
 }

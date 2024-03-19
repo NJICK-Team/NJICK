2c1fd86279b6058a0bb2e0d4d90ef9cc9b65dfce
kimpangya <nam74415133@gmail.com>
2024-03-20 07:17:03 +0900

    ✨feat/comments : 카드댓글 조회 구현

--- src/main/java/com/sparta/njick/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/njick/domain/comment/controller/CommentController.java
@@ -1,13 +1,17 @@
 package com.sparta.njick.domain.comment.controller;
 
 import com.sparta.njick.domain.comment.dto.requestDto.CommentRequestDto;
+import com.sparta.njick.domain.comment.dto.responseDto.CommentResponseDto;
 import com.sparta.njick.domain.comment.service.CommentService;
 import com.sparta.njick.domain.taskState.common.CommonResponseDto;
 import com.sparta.njick.domain.user.userDetails.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +34,17 @@ public class CommentController {
     ) {
         commentService.createComment(requestDto, boardId, cardId,
             userDetails.getUser().getUserId());
+        return CommonResponseDto.of(HttpStatus.OK, "카드댓글 생성 성공", null);
     }
 
+    @GetMapping
+    public ResponseEntity<CommonResponseDto<List<CommentResponseDto>>> getComments(
+        @PathVariable Long boardId,
+        @PathVariable Long cardId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        List<CommentResponseDto> responseDto = commentService.getComments(boardId, cardId,
+            userDetails.getUser().getUserId());
+        return CommonResponseDto.of(HttpStatus.OK, "카드댓글 조회 성공", responseDto);
+    }
 }
--- src/main/java/com/sparta/njick/domain/comment/repository/CommentJpaRepository.java
+++ src/main/java/com/sparta/njick/domain/comment/repository/CommentJpaRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.njick.domain.comment.repository;
 
 import com.sparta.njick.domain.comment.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findAllByCardId(Long cardId);
 }

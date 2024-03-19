63f53a416025e039349f123de893027d34a108ac
DBuu_Kong <feel00k0810@gmail.com>
2024-03-20 00:44:06 +0900

    refactor: 익셉션 핸들러 추가

--- src/main/java/com/sparta/njick/global/exception/GlobalCustomExceptionHandler.java
+++ src/main/java/com/sparta/njick/global/exception/GlobalCustomExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.sparta.njick.global.exception;
 
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -25,5 +29,24 @@ public class GlobalCustomExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(new ErrorResponse(
                 Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
+    }
+
+    @ExceptionHandler({
+        IllegalArgumentException.class,
+        NoSuchElementException.class,
+        EntityNotFoundException.class
+    })
+    public ResponseEntity<ErrorResponse> BadRequestExceptionHandler(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler({
+        EntityExistsException.class,
+        DuplicateKeyException.class
+    })
+    public ResponseEntity<ErrorResponse> ConflictExceptionHandler(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 }

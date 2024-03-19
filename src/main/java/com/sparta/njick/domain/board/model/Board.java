325f7c5796e83e74ad27e4a500721c2e38a32901
Modafi <dkdpdldjdy0619@gmail.com>
2024-03-19 18:25:53 +0900

    feat: implement Board model class

--- src/main/java/com/sparta/njick/domain/board/model/Board.java
+++ src/main/java/com/sparta/njick/domain/board/model/Board.java
@@ -1,15 +1,42 @@
 package com.sparta.njick.domain.board.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.sparta.njick.global.exception.CustomRuntimeException;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Board {
-    // TODO: implement Board domain class
+    private Long id;
+    private String title;
+    private String description;
+    private String color;
+    private Long creatorId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 
+    public boolean isNotOwner(Long requestUserId) {
+        return !this.creatorId.equals(requestUserId);
+    }
+
+    public Board update(String title, String description, String color, Long requestUserId) {
+        if (this.isNotOwner(requestUserId)) {
+            throw new CustomRuntimeException("[ERROR] 권한이 없습니다");
+        }
+
+        return Board.builder()
+                .id(this.id)
+                .title(title)
+                .description(description)
+                .color(color)
+                .creatorId(this.creatorId)
+                .createdAt(this.createdAt)
+                .updatedAt(this.updatedAt)
+                .deletedAt(this.deletedAt)
+                .build();
+    }
 }

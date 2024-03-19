648abee07525b4000443dc32b5ce65df42502d31
DBuu_Kong <feel00k0810@gmail.com>
2024-03-19 23:36:47 +0900

    feat: 유저 정보변경 기능 구현

--- src/main/java/com/sparta/njick/domain/user/entity/User.java
+++ src/main/java/com/sparta/njick/domain/user/entity/User.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -25,9 +26,12 @@ public class User extends BaseAuditing {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Setter
     @Column(nullable = false)
     private String password;
 
+    @Setter
     @Column(nullable = false, unique = true)
     private String nickname;
+
 }

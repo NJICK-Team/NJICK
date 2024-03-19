a68facab942938abb58cf66fa907c9ed048654da
DBuu_Kong <feel00k0810@gmail.com>
2024-03-19 20:18:39 +0900

    fix: 인코딩 오류 수정

--- src/main/java/com/sparta/njick/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/njick/global/jwt/JwtAuthenticationFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Slf4j(topic = "JWT 인증")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 

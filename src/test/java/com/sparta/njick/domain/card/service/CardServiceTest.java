5fe2946e771f3298243775646871c56464f7ac4e
jinkshower <askthem@naver.com>
2024-03-19 22:06:24 +0900

    test: card service 조회 테스트

--- src/test/java/com/sparta/njick/domain/card/service/CardServiceTest.java
+++ src/test/java/com/sparta/njick/domain/card/service/CardServiceTest.java
@@ -8,12 +8,14 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.times;
 import static org.mockito.BDDMockito.verify;
 
+import com.sparta.njick.domain.assign.model.Assign;
 import com.sparta.njick.domain.card.dto.response.CardResponseDto;
 import com.sparta.njick.domain.card.fixture.CardFixture;
 import com.sparta.njick.domain.card.infrastructure.entity.CardEntity;
 import com.sparta.njick.domain.card.model.Card;
 import com.sparta.njick.domain.card.repository.CardRepository;
 import com.sparta.njick.domain.card.repository.CardRepositoryImpl;
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -32,16 +34,34 @@ class CardServiceTest implements CardFixture {
     @Mock
     CardRepositoryImpl cardRepository;
 
-    @DisplayName("카드 생성")
+    @DisplayName("카드 생성 성공")
     @Test
     void createCard() {
         //given
         given(cardRepository.save(any(Card.class))).willReturn(TEST_CARD_INFO_DTO);
 
-        //when & then
+        //when
         CardResponseDto result = cardService.createCard(TEST_CREATE_REQUEST_DTO, TEST_BOARD_ID,
             TEST_USER_ID);
+
+        //then
         assertThat(result.getTitle()).isEqualTo(TEST_TITLE);
         assertThat(result.getAssignedUserIds().size()).isEqualTo(3);
+    }
+
+    @DisplayName("카드 조회 성공")
+    @Test
+    void getCard() {
+        //given
+        given(cardRepository.get(eq(TEST_CARD_ID))).willReturn(TEST_CARD);
+        given(cardRepository.findAssignsByCardId(eq(TEST_CARD_ID))).willReturn(
+            List.of(new Assign(1L, 1L), new Assign(2L, 1L)));
+
+        //when
+        CardResponseDto result = cardService.getCard(TEST_USER_ID, TEST_BOARD_ID, TEST_CARD_ID);
+
+        //then
+        assertThat(result.getTitle()).isEqualTo(TEST_TITLE);
+        assertThat(result.getAssignedUserIds().size()).isEqualTo(2);
     }
 }

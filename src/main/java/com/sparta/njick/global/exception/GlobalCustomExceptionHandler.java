38de38b6686a34342805d7d2e6bf61e8a6108444
DBuu_Kong <feel00k0810@gmail.com>
2024-03-19 19:01:10 +0900

    feat: 예외 핸들러 추가

--- src/main/java/com/sparta/njick/global/exception/GlobalCustomExceptionHandler.java
+++ src/main/java/com/sparta/njick/global/exception/GlobalCustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.sparta.njick.global.exception;
 
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,5 +17,14 @@ public class GlobalCustomExceptionHandler {
         log.error(e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(
+                Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()));
     }
 }

255392f41fe86090667af92faafdffb961b86256
jinkshower <askthem@naver.com>
2024-03-20 12:39:14 +0900

    feat: 보드 참여 유저 검증 로직 추가

--- src/main/java/com/sparta/njick/domain/card/service/CardServiceImpl.java
+++ src/main/java/com/sparta/njick/domain/card/service/CardServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sparta.njick.domain.card.service;
 
 import com.sparta.njick.domain.assign.model.Assigns;
+import com.sparta.njick.domain.board.repository.BoardRepository;
 import com.sparta.njick.domain.card.dto.request.CardCreateRequestDto;
 import com.sparta.njick.domain.card.dto.request.CardUpdateRequestDto;
 import com.sparta.njick.domain.card.dto.response.CardResponseDto;
 import com.sparta.njick.domain.card.model.Card;
 import com.sparta.njick.domain.card.repository.CardRepository;
+import com.sparta.njick.global.exception.CustomRuntimeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class CardServiceImpl implements CardService {
 
     private final CardRepository cardRepository;
+    private final BoardRepository boardRepository;
 
     @Override
     @Transactional
     public CardResponseDto createCard(CardCreateRequestDto requestDto, Long boardId, Long userId) {
-        //TODO: 보드에 초대된 사용자인지 검증
+        if (!boardRepository.isParticipated(boardId, userId)) {
+            throw new CustomRuntimeException("해당 보드에 참여중인 유저가 아닙니다.");
+        }
 
         Card card = cardRepository.save(requestDto, boardId, userId);
         Assigns assigns = cardRepository.assignAll(
@@ -30,7 +35,9 @@ public class CardServiceImpl implements CardService {
     @Override
     @Transactional
     public CardResponseDto getCard(Long userId, Long boardId, Long cardId) {
-        //TODO: 보드에 초대된 사용자인지 검증
+        if (!boardRepository.isParticipated(boardId, userId)) {
+            throw new CustomRuntimeException("해당 보드에 참여중인 유저가 아닙니다.");
+        }
 
         Card found = cardRepository.get(cardId);
         found.validateBoardId(boardId);
@@ -44,7 +51,10 @@ public class CardServiceImpl implements CardService {
     @Transactional
     public CardResponseDto updateCard(CardUpdateRequestDto requestDto, Long boardId, Long cardId,
         Long userId) {
-        //TODO: 보드에 초대된 사용자인지 검증
+        if (!boardRepository.isParticipated(boardId, userId)) {
+            throw new CustomRuntimeException("해당 보드에 참여중인 유저가 아닙니다.");
+        }
+
         Card found = cardRepository.get(cardId);
         Card updateCard = found.update(requestDto, boardId);
         Card updated = cardRepository.update(updateCard);

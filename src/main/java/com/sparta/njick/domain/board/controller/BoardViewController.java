ec62624bf0427168dc384cf52efc01dda3b94023
Modafi <dkdpdldjdy0619@gmail.com>
2024-03-21 17:05:44 +0900

    feat: implement Board view template

---
+++ src/main/java/com/sparta/njick/domain/board/controller/BoardViewController.java
@@ -0,0 +1,19 @@
+package com.sparta.njick.domain.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/boards")
+public class BoardViewController {
+
+    @GetMapping("/{boardId}")
+    public String getBoard(@PathVariable("boardId") Long boardId, Model model) {
+        System.out.println("Board Id: " + boardId);
+        model.addAttribute("boardId", boardId);
+        return "/board/boardView";
+    }
+}

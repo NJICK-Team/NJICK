ed0216d18b2867b74966bb25f63c0ab3b6242299
DBuu_Kong <feel00k0810@gmail.com>
2024-03-21 10:06:47 +0900

    refactor: 컨트롤러 반환값 변경

--- src/main/java/com/sparta/njick/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/njick/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sparta.njick.domain.user.controller;
 
+import com.sparta.njick.domain.taskState.common.CommonResponseDto;
 import com.sparta.njick.domain.user.dto.request.NicknameUpdateRequest;
 import com.sparta.njick.domain.user.dto.request.PasswordUpdateRequest;
 import com.sparta.njick.domain.user.dto.request.SignUpRequest;
@@ -9,6 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -27,30 +30,40 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public void signup(@Valid @RequestBody SignUpRequest request) {
+    public ResponseEntity<CommonResponseDto<Void>> signup(
+        @Valid @RequestBody SignUpRequest request) {
         userService.signup(request);
+        return CommonResponseDto.of(HttpStatus.OK, "회원가입 성공", null);
     }
 
     @PostMapping("/logout")
-    public void logout(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<CommonResponseDto<Void>> logout(HttpServletRequest request,
+        HttpServletResponse response) {
         new SecurityContextLogoutHandler().logout(request, response,
             SecurityContextHolder.getContext().getAuthentication());
+        return CommonResponseDto.of(HttpStatus.OK, "로그아웃 성공", null);
     }
 
     @PatchMapping("/updates/password")
-    public void updatePassword(@Valid @RequestBody PasswordUpdateRequest request,
+    public ResponseEntity<CommonResponseDto<Void>> updatePassword(
+        @Valid @RequestBody PasswordUpdateRequest request,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         userService.updatePassword(request, userDetails.getUser());
+        return CommonResponseDto.of(HttpStatus.OK, "비밀번호 변경 성공", null);
     }
 
     @PatchMapping("/updates/nickname")
-    public void updateNickname(@Valid @RequestBody NicknameUpdateRequest request,
+    public ResponseEntity<CommonResponseDto<Void>> updateNickname(
+        @Valid @RequestBody NicknameUpdateRequest request,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         userService.updateNickname(request, userDetails.getUser());
+        return CommonResponseDto.of(HttpStatus.OK, "닉네임 변경 성공", null);
     }
 
     @DeleteMapping("/delete")
-    public void delete(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<CommonResponseDto<Void>> delete(
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
         userService.delete(userDetails.getUser());
+        return CommonResponseDto.of(HttpStatus.OK, "회원탈퇴 성공", null);
     }
 }

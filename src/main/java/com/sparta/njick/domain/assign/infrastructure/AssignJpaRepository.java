056ddc9b350fd54bd7ebae65b4b1d8a6d5a1245c
jinkshower <askthem@naver.com>
2024-03-22 15:50:30 +0900

    feat: jpql 추가

--- src/main/java/com/sparta/njick/domain/assign/infrastructure/AssignJpaRepository.java
+++ src/main/java/com/sparta/njick/domain/assign/infrastructure/AssignJpaRepository.java
@@ -2,10 +2,16 @@ package com.sparta.njick.domain.assign.infrastructure;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AssignJpaRepository extends JpaRepository<AssignEntity, Long> {
 
+    @Query("select a from AssignEntity a where a.cardId = :cardId")
     List<AssignEntity> findAllByCardId(Long cardId);
 
-    void deleteAllByCardId(Long cardId);
+    @Modifying
+    @Query("delete from AssignEntity a where a.cardId = :cardId")
+    void deleteAllByCardId(@Param("cardId") Long cardId);
 }
